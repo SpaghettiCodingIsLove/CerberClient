2614072dd47d3c57c237afca3044ab5775454a44
Adam Wolszleger <43927273+sylfero@users.noreply.github.com>
2021-01-09 17:13:19 +0100

    Update RegisterRequest.cs

--- CerberClient/Model/Api/RegisterRequest.cs
+++ CerberClient/Model/Api/RegisterRequest.cs
@@ -6,8 +6,6 @@
 
         public string LastName { get; set; }
 
-        public string Image { get; set; }
-
         public string Email { get; set; }
 
         public string Password { get; set; }
