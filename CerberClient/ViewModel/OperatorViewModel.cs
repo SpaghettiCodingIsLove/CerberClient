6c3ac36d34e0c11dc2b54670b882964dc88c3ac2
zima99 <62467321+zima99@users.noreply.github.com>
2021-01-12 17:19:30 +0100

    Operator view i vm

---
+++ CerberClient/ViewModel/OperatorViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using CerberClient.ViewModel.BaseClasses;
+using CerberClient.View;
+using System.ComponentModel;
+using System.Windows.Input;
+using CerberClient.Model.Api;
+
+namespace CerberClient.ViewModel
+{
+    class OperatorViewModel : ViewModelBase
+    {
+        private MainViewModel mainViewModel = new MainViewModel();
+
+        private string name;
+        private string lastName;
+        private string organisationName;
+        private List<UserResponse> userResponseList;
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                lastName = value;
+                OnPropertyChanged(nameof(LastName));
+            }
+        }
+
+        public string OrganisationName
+        {
+            get => organisationName;
+            set
+            {
+                organisationName = value;
+                OnPropertyChanged(nameof(OrganisationName));
+            }
+        }
+        public List<UserResponse> UserResponseList
+        {
+            get => userResponseList;
+            set
+            {
+                userResponseList = value;
+            }
+        }
+        
+    }
+}
